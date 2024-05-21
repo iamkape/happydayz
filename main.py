10b414418ccac58510e6f76b1bd0b2c70a8424d3
unotuno <iamkape@mail.ru>
2024-05-21 11:16:16 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+# -------------- Секунды в формат d:h:m:s
+
+# seconds = int(input("Введи число секунд:"))
+# days = seconds // 86400
+# new_seconds =  seconds % 86400
+# hours = new_seconds // 3600
+# minute = new_seconds // 60
+# sec = new_seconds % 60
+# print(f"{days}d : {hours}h : {minute}m : {sec}s")
+
+# Время 21 минута.
+
+
+
+# -------------- Посчитайте n+nn+nnn.
+
+# n = int(input())
+# result = n + pow(n,2) + pow(n,3)
+# print(result)
+
+# Время 2 минуты.
+
+
+#------------------------Самое частое слово и длинное найти
+
+# text = input().split()
+# words = {}
+# for word in text:
+#     if word in words:
+#         words[word] += 1
+#     else:
+#         words[word] = 1
+# identical_word = (max(word))
+# print(f"Часто встречается слово: {identical_word}")
+#
+# max_long_word = (max(text, key = len))
+# print (f"Максимально длинное слово: {max_long_word}")
+
+# больше 30 минут ;( намного больше
+
+
+
+
+
+
+
